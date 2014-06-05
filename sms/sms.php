9a13ad428c1fe2a8d924346f83f502df2b5d838d
George Botley <George@Mac-2.local>
2014-06-05 11:14:05 +0100

    Created sms package and sms.php file

---
+++ sms/sms.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * sms.php - Twilio
+ * 
+ * Interact with Twilio API SMS facility.
+ *
+ * @author George Botley <george@torindul.co.uk>
+ * @version 4301410344
+ * @copyright Copyright © 2014, Torindul Business Solutions
+ * @package sms
+ *
+ */
+ ?>
