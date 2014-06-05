6791ca71a9311de288fb01aa46088560638ef553
George Botley <George@Mac-2.local>
2014-06-05 12:18:43 +0100

    Added the API_LOCATION

--- config/config.php
+++ config/config.php
@@ -14,10 +14,12 @@
  
 /**
  * API Configuration Options
- * 
+ *
+ * - API_LOCATION = The URI of the parent folder where these API files reside.
  * - API_ACCOUNT_SID = The account SID hash. Can be found in the Twilio dashboard.
  * - API_URL = The url in which to communicate with the API.
  */
+ define('API_LOCATION', 'http://www.torindul.co.uk/twilio');
  define('API_ACCOUNT_SID', 'AC718634fa7cc8856c75633f50526c0141'); 
  define('API_URL', 'https://api.twilio.com/2010-04-01'); 
  
