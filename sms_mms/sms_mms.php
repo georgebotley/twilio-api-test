e45c4afc87c5d4814929ebb2144debc46c35d4f0
George Botley <George@Mac-2.local>
2014-06-05 11:17:35 +0100

    Renamed sms package to sms_mms inline with Twilio API

---
+++ sms_mms/sms_mms.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * sms_mms.php - Twilio
+ * 
+ * Interact with Twilio API SMS and MMS facility.
+ *
+ * @author George Botley <george@torindul.co.uk>
+ * @version 4301410344
+ * @copyright Copyright © 2014, Torindul Business Solutions
+ * @package sms_mms
+ *
+ */
+ class sms_mms {
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+ }
+ 
+ 
+ ?>
