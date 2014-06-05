bea8e3aa30003167b273da994435d68f7d70c899
George Botley <George@Mac-2.local>
2014-06-05 10:55:05 +0100

    Added _config directory Added _config/_config.php file

---
+++ _config/_config.php
@@ -0,0 +1,11 @@
+/**
+ * _config.php - Twilio
+ * 
+ * Twilio API Configuration
+ *
+ * @author George Botley <george@torindul.co.uk>
+ * @version 4301410344
+ * @copyright Copyright © 2014, Torindul Business Solutions
+ * @package _config
+ *
+ */
