f46728523b0526658bee5965a3f68912fe3c1b95
George Botley <George@Mac-2.local>
2014-06-05 12:07:00 +0100

    Created the controller

---
+++ core/controller.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * controller.php - Twilio
+ * 
+ * Control the operation of the API calls and callbacks.
+ *
+ * @author George Botley <george@torindul.co.uk>
+ * @version 4301410344
+ * @copyright Copyright © 2014, Torindul Business Solutions
+ * @package core
+ *
+ */
+ 
+ echo "hello";
+ 
+?>
