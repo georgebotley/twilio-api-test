03a58bac4bdc43cacda7d03619121656861475fe
George Botley <George@Mac-2.local>
2014-06-05 12:03:49 +0100

    Created index.php and referenced the initialiser ./core/init.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * index.php - Twilio
+ * 
+ * Include the API Initialiser.
+ *
+ * @author George Botley <george@torindul.co.uk>
+ * @version 4301410344
+ * @copyright Copyright © 2014, Torindul Business Solutions
+ * @package /
+ *
+ */
+ 
+ //Include the initialiser.
+ include_once "./core/init.php";
+
+?>
