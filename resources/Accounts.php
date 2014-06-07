345acda869262d11a6cd7780f62adfd79c29e96b
George Botley <george@torindul.co.uk>
2014-06-07 23:44:48 +0100

    Introduced the Accounts API

---
+++ resources/Accounts.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Accounts.php - Twilio
+ * 
+ * Interface with the primary account.
+ *
+ * @author George Botley <george@torindul.co.uk>
+ * @version 4301410344
+ * @copyright Copyright © 2014, Torindul Business Solutions
+ * @package Twilio
+ * @subpackage resources
+ *
+ */
+ 
+ ?>
