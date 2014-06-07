64aaade1bc3808ac2d4cb50c7b3b5574ecc47f1a
George Botley <george@torindul.co.uk>
2014-06-07 13:30:55 +0100

    Added API token and PHP errors config option

--- config/config.php
+++ config/config.php
@@ -16,13 +16,18 @@
 /**
  * API Configuration Options
  *
- * - API_LOCATION = The URI of the parent folder where these API files reside.
- * - API_ACCOUNT_SID = The account SID hash. Can be found in the Twilio dashboard.
- * - API_URL = The url in which to communicate with the API.
+ * - API_LOCAL_LOCATION 		= The URI of the parent folder where the local API files reside, excluding the protocol.
+ * - API_REMOTE_LOCATION 		= The url in which to communicate with the API, excluding the protocol.
+ * - API_ACCOUNT_SID 			= The account SID hash. Can be found in the Twilio dashboard.
+ * - API_ACCOUNT_TOKEN 			= The account token. Can be found in the Twilio dashboard.
+ * - PHP_ERRORS 				= Shall we show PHP errors? 1 = yes, 0 = no.
  */
- define('API_LOCATION', 'http://www.torindul.co.uk/twilio');
+ define('API_LOCAL_LOCATION', 'www.torindul.co.uk/twilio');
+ define('API_REMOTE_LOCATION', 'api.twilio.com/2010-04-01'); 
+ define('API_PROTOCOL', 'https://');
  define('API_ACCOUNT_SID', 'AC718634fa7cc8856c75633f50526c0141'); 
- define('API_URL', 'https://api.twilio.com/2010-04-01'); 
+ define('API_ACCOUNT_TOKEN', 'f84db14b022fa60fe33339e7f28cc07a'); 
+ define('PHP_ERRORS', 1);
  
  
  ?>
