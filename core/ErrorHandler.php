499e1221c85fb359ae049b2aef736a0a58796736
George Botley <george@torindul.co.uk>
2014-06-08 01:12:54 +0100

    Introduced HTTP error checking

--- core/ErrorHandler.php
+++ core/ErrorHandler.php
@@ -30,6 +30,87 @@
 		 
 	 }	 
 	 
+	 
+	 /**
+	  * HTTPStatus function.
+	  *
+	  * Check the status code of an API response. Return true or false based on status code meaning.
+	  *
+	  * (https://www.twilio.com/docs/api/rest/request)
+	  * 
+	  * @access public
+	  * @param mixed $status
+	  * @return void
+	  */
+	 public function HTTPStatus($status) {
+		 
+		 switch($status) {
+			 
+			 //200 - OK
+			 //The request was successful and the response body contains the representation requested.
+			 case 200:
+			 	return true;
+			 break;
+			 
+			 //201 - CREATED
+			 //The request was successful, we updated the resource and the response body contains the representation.
+			 case 201:
+			 	return true;
+			 break;
+			 
+			 //302 - FOUMD
+			 //A common redirect response; you can GET the representation at the URI in the Location response header.
+			 case 302:
+			 	return true;
+			 break;
+			 
+			 //304 - NOT MODIFIED
+			 //Your client's cached version of the representation is still up to date.
+			 case 304:
+			 	return true;
+			 break;
+			 
+			 //401 - UNAUTHORIZED
+			 //The supplied credentials, if any, are not sufficient to access the resource.
+			 case 401:
+			 	$this->throwError("HTTP Status Code 401 - Uh Oh! It looks like the credentials you're using are invalid. Please correct them in config/config.php");
+			 break;
+			 
+			 //404 - NOT FOUND
+			 //Resource not found.
+			 case 404:
+			 	$this->throwError("HTTP Status Code 404 - The resource you required could not be found. Contact your system administrator.");
+			 break;
+			 
+			 //405 - NOT ALLOWED
+			 //You can't POST or PUT to the resource.
+			 case 405:
+			 	$this->throwError("HTTP Status Code 405 - You can't POST or PUT to the resource requested.");
+			 break;			 
+			 
+			 //429 - TOO MANY REQUESTS
+			 //Your application is sending too many simultaneous requests.
+			 case 429:
+			 	$this->throwError("HTTP Status Code 429 - Too many simultaneous requests from you. Allow me to catch up. Please try again shortly.");
+			 break;
+			 
+			 //500 - SERVER ERROR
+			 //We couldn't return the representation due to an internal server error.
+			 case 500:
+			 	$this->throwError("HTTP Status Code 500 - An internal server error has occured. Please try again shortly.");
+			 break;
+			 
+			 //503 - SERVICE UNAVAILABLE
+			 //We are temporarily unable to return the representation. Please wait for a bit and try again.
+			 case 503:
+			 	$this->throwError("HTTP Status Code 503 - Resource temporarily unavailable. Please try again shortly.");
+			 break;			 			 
+			 
+			 
+		 } 
+		 
+	 }
+	 
  }
  
  ?>
