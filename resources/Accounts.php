b0a23c361ef5decc179569bc63a178d0dbaba9d4
George Botley <george@torindul.co.uk>
2014-06-08 01:15:23 +0100

    .

--- resources/Accounts.php
+++ resources/Accounts.php
@@ -11,5 +11,48 @@
  * @subpackage resources
  *
  */
+ class Accounts extends Communicator {
+ 
+ 	 /**
+	  * resource
+	  *
+	  * The REST API resource for the sms and mms capability.
+	  *
+	  * REQUIRED BY THIS RESOURCE
+	  * 
+	  * @var string
+	  * @access private
+	  */
+	 private $api_resource;
+	 
+	 /**
+	  * getAccount function.
+	  *
+	  * Retreive an accounts details by Sid.
+	  * 
+	  * @access public
+	  * @param mixed $Sid - An account to query, using Sid.
+	  * @return void
+	  */
+	 public function getAccount($Sid=null) {
+	 
+	 	//Establish the resource
+	 	$this->api_resource = '/Accounts/' . $Sid;
+	 	
+		//Form the REST Request URI
+		$requestURI = $this->api_protocol . $this->api_location . $this->api_resource;	
+		
+		//Store each parameter in an array for REST preparation
+		$requestParameters = array();
+		
+		//Append $requestParameters to our REST Request URI and return it out of the function
+		return array( $requestParameters, $requestURI );
+	 
+	 }
+
+	 
+	 
+	 
+ }
  
  ?>
