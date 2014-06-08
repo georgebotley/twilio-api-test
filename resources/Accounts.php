1fa537d9f6fb9e5cb4fb576f7a7962cc343b628b
George Botley <george@torindul.co.uk>
2014-06-08 21:31:25 +0100

    Changed method names as per SilverStripe coding conventions. Also called the Communicator via the parent:: call instead of needing to call it outside of method

--- resources/Accounts.php
+++ resources/Accounts.php
@@ -2,7 +2,7 @@
 /**
  * Accounts.php - Twilio
  * 
- * Interface with the master account.
+ * Interact with the master account resources and sub accounts.
  *
  * @author George Botley <george@torindul.co.uk>
  * @version 4301410344
@@ -14,150 +14,137 @@
  *
  */
  class Accounts extends Communicator {
- 
- 	 /**
-	  * resource
-	  *
-	  * The REST API resource for the sms and mms capability.
-	  *
-	  * REQUIRED BY THIS RESOURCE
-	  * 
-	  * @var string
-	  * @access private
-	  */
-	 private $api_resource;
 	 
 	 /**
-	  * getAccount function.
+	  * get_account function.
 	  *
 	  * Retreive an accounts details by Sid.
 	  * 
 	  * @access public
-	  * @param mixed $Sid - An account to query, using Sid.
-	  * @return void
+	  *
+	  * @method $Accounts->get_account( $Sid );
+	  *
+	  * @param mixed $Sid - An account to query, using Sid. Defaults to the master Sid if left blank.
+	  *
+	  * @return XML response from API.
 	  */
-	 public function getAccount($Sid=null) {
+	 public function get_account( $Sid=API_ACCOUNT_SID ) {
 	 
 	 	//Establish the resource
-	 	$this->api_resource = '/Accounts/' . $Sid;
+	 	$resource = '/Accounts/' . $Sid;
 	 	
 		//Form the REST Request URI
-		$requestURI = $this->api_protocol . $this->api_location . $this->api_resource;	
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
 		
-		//Store each parameter in an array for REST preparation
-		$requestParameters = array();
-		
-		//Append $requestParameters to our REST Request URI and return it out of the function
-		return array( $requestParameters, $requestURI );
+	 	//Send the request and return the result as XML
+	 	return parent::send_request( array(), $requestURI, "GET", DEBUG_MODE, DUMMY_MODE );
 	 
 	 }
 	 	 
 	 /**
-	  * updateAccount function.
+	  * update_account function.
 	  *
-	  * Update the account
+	  * Update an accounts details
 	  * 
 	  * @access public
-	  * @param mixed $Sid - Account Resource ID
-	  * @param mixed $FriendlyName (default: null)
-	  * @return void
+	  *
+	  * @param array $Params 	- An array of parameters, e.g, (ParameterName => Value), as at https://www.twilio.com/docs/api/rest/account
+	  * @param mixed $Sid 		- An account to update, using Sid. Defaults to the master Sid if left blank.
+	  *
+	  * @return XML response from API.
 	  */
-	 public function updateAccount($Sid=null, $FriendlyName=null) {
+	 public function update_account( $Params = array(), $Sid=API_ACCOUNT_SID ) {
 		 
 	 	//Establish the resource
-	 	$this->api_resource = '/Accounts/' . $Sid;
+	 	$resource = '/Accounts/' . $Sid;
 	 	
 		//Form the REST Request URI
-		$requestURI = $this->api_protocol . $this->api_location . $this->api_resource;	
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
 		
-		//Store each parameter in an array for REST preparation
-	 	$requestParameters = array("FriendlyName" => $FriendlyName);
-	 	
-		//Append $requestParameters to our REST Request URI and return it out of the function
-		return array( $requestParameters, $requestURI );
+	 	//Send the request and return the result as XML
+	 	return parent::send_request( $Params, $requestURI, "POST", DEBUG_MODE, DUMMY_MODE );
 		 
 	 }
 	 
 	 /**
-	  * updateAccount function.
+	  * update_account_status function.
 	  *
 	  * Update the account
 	  * 
 	  * @access public
-	  * @param mixed $Sid - Account Resource ID
+	  *
 	  * @param mixed $Status - the accounts status. Defaults to active. Can be either suspended or closed. The latter being destructive and irreversible.
-	  * @return void
+	  * @param mixed $Sid - An account to update, using Sid. Can not be the master account Sid.
+	  *
+	  * @return XML response from API.
 	  */
-	 public function updateAccountStatus($Sid=null, $Status="active") {
+	 public function update_account_status( $Status='Active', $Sid ) {
 	 
-	 	//If the $Sid belongs to the master account then fail.
-	 	if($Sid==API_ACCOUNT_SID) { $this->throwError("Status can not be updated on the primary account."); exit(); }
+	 	//If the $Sid belongs to the master account then fail
+	 	if($Sid==API_ACCOUNT_SID) { $this->throwError("Status can not be updated on the primary account.", __FILE__, __LINE__); }
 		 
 	 	//Establish the resource
-	 	$this->api_resource = '/Accounts/' . $Sid;
+	 	$resource = '/Accounts/' . $Sid;
 	 	
 		//Form the REST Request URI
-		$requestURI = $this->api_protocol . $this->api_location . $this->api_resource;	
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
 		
 		//Store each parameter in an array for REST preparation
 	 	$requestParameters = array("Status" => $Status);
 	 	
-		//Append $requestParameters to our REST Request URI and return it out of the function
-		return array( $requestParameters, $requestURI );
+		//Send the request and return the result as XML
+	 	return parent::send_request( $requestParameters, $requestURI, "POST", DEBUG_MODE, DUMMY_MODE );
 		 
 	 }
 	 
 	 /**
-	  * searchAccounts function.
+	  * search_accounts function.
 	  *
 	  * Search the database of accounts that belong to the account making this API call.
 	  * 
 	  * @access public
-	  * @param mixed $FriendlyName - Only return the Account resources with friendly names that exactly match this name.
-	  * @param mixed $Status - Only return Account resources with the given status. Can be closed, suspended or active.
-	  * @return void
+	  *
+	  * @param array $Params 	- An array of parameters, e.g, (ParameterName => Value), as at https://www.twilio.com/docs/api/rest/account#list-get-filters
+	  *
+	  * @return XML response from API.
 	  */
-	 public function searchAccounts($FriendlyName=null, $Status=null) {
+	 public function search_accounts( $Params = array() ) {
 		 
 	 	//Establish the resource
-	 	$this->api_resource = '/Accounts';
+	 	$resource = '/Accounts';
 	 		 	
 		//Form the REST Request URI
-		$requestURI = $this->api_protocol . $this->api_location . $this->api_resource;	
-		
-		//Store each parameter in an array for REST preparation
-	 	$requestParameters = array(
-	 								"FriendlyName" 	=> $FriendlyName,
-	 								"Status" 		=> $Status
-	 						);
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
 	 	
 		//Append $requestParameters to our REST Request URI and return it out of the function
-		return array( $requestParameters, $requestURI );
+		return parent::send_request( $Params, $requestURI, "GET", DEBUG_MODE, DUMMY_MODE );
 		 
 	 }
 	 
 	 /**
-	  * createSubAccount function.
+	  * create_sub_account function.
 	  *
 	  * Create a sub account for the purposes of billing restrictions.
 	  * 
 	  * @access public
+	  *
 	  * @param mixed $FriendlyName - A human readable description of the new subaccount, up to 64 characters. Defaults to "SubAccount Created at {YYYY-MM-DD HH:MM}".
-	  * @return void
+	  *
+	  * @return XML response from API.
 	  */
-	 public function createSubAccount($FriendlyName=null) {
+	 public function create_sub_account($FriendlyName=null) {
 		 
 	 	//Establish the resource
-	 	$this->api_resource = '/Accounts';
+	 	$resource = '/Accounts';
 	 		 	
 		//Form the REST Request URI
-		$requestURI = $this->api_protocol . $this->api_location . $this->api_resource;	
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
 		
 		//Store each parameter in an array for REST preparation
-	 	$requestParameters = array("FriendlyName" 	=> $FriendlyName);
+	 	$Params = array("FriendlyName" 	=> $FriendlyName);
 	 	
 		//Append $requestParameters to our REST Request URI and return it out of the function
-		return array( $requestParameters, $requestURI );
+		return parent::send_request( $Params, $requestURI, "POST", DEBUG_MODE, DUMMY_MODE );
 		 
 	 }
 	 
