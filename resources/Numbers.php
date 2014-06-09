dcaeee8a9eb99f1eb94e2eb0f7bb13ad87e29911
George Botley <george@torindul.co.uk>
2014-06-09 12:44:02 +0100

    Introduced account_transfer() method. Also finalised callerid methods.

--- resources/Numbers.php
+++ resources/Numbers.php
@@ -163,7 +163,7 @@
 	  *
 	  * @method $Numbers->release_number( $PhoneNumberSid, $Sid );
 	  *
-	  * @param string $PhoneNumberSid - The Sid of the phone number we are to delete.
+	  * @param string $PhoneNumberSid - The Sid of the phone number we are to release.
 	  * @param string $Sid	 - The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
 	  *
 	  * @return XML response from API.
@@ -178,6 +178,37 @@
 		 
 		//Send the request and return the result as XML
 		return parent::send_request( array(), $requestURI, "DELETE", DEBUG_MODE, DUMMY_MODE );		 
+		 
+	 }
+	 
+	 /**
+	  * account_transfer function.
+	  *
+	  * Transfer a number between accounts
+	  * 
+	  * @access public
+	  *
+	  * @method $Numbers->account_transfer( $NewAccountSid, $PhoneNumberSid, $Sid );
+	  *
+	  * @param string $NewAccountSid - The Sid of the account we are to transfer the number to.
+	  * @param string $PhoneNumberSid - The Sid of the phone number we are to transfer.
+	  * @param string $Sid	 - The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
+	  *
+	  * @return XML response from API.
+	  */
+	 public function account_transfer( $NewAccountSid, $PhoneNumberSid, $Sid=API_ACCOUNT_SID ) {
+		 
+		//Establish the resource
+		$resource = '/Accounts/ ' . $Sid . '/IncomingPhoneNumbers/ ' . $PhoneNumberSid;
+					 	
+		//Form the REST Request URI
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
+		
+		//Merge the arrays
+		$Params = array_merge( array('AccountSid' => $NewAccountSid), array() );
+		 
+		//Send the request and return the result as XML
+		return parent::send_request( array(), $requestURI, "POST", DEBUG_MODE, DUMMY_MODE );		 
 		 
 	 }
 	 
@@ -218,8 +249,8 @@
 	  * @method $Numbers->create_outgoingcallerid( '+441234123456', array(), $Sid );
 	  *
 	  * @param string $PhoneNumber - The phone number to verify. Should be formatted with a '+' and country code e.g., +441234123456
-	  * @param array $Params 	- An array e.g, (ParameterName => Value). See https://www.twilio.com/docs/api/rest/outgoing-caller-ids#list-post-optional-parameters
-	  * @param string $Sid	 	- The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
+	  * @param array $Params - An array e.g, (ParameterName => Value). See https://www.twilio.com/docs/api/rest/outgoing-caller-ids#list-post-optional-parameters
+	  * @param string $Sid - The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
 	  *
 	  * @return XML response from API.
 	  */
@@ -238,9 +269,91 @@
 		return parent::send_request( $Params, $requestURI, "POST", DEBUG_MODE, DUMMY_MODE ); 
 		 
 	 }
+
+ 	 /**
+	  * delete_outgoingcallerid function.
+	  *
+	  * Deletes a specified outgoing caller id.
+	  * 
+	  * @access public 
+	  *
+	  * @method $Numbers->delete_outgoingcallerid( $CallerIDSid );
+	  *
+	  * @param string $CallerIDSid - The Sid of the outgoing caller id which we are to delete.
+	  * @param string $Sid - The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
+	  *
+	  * @return XML response from API.
+	  */
+	 public function delete_outgoingcallerid( $CallerIDSid, $Sid=API_ACCOUNT_SID ) {
+		 
+		//Establish the resource
+		$resource = '/Accounts/' . $Sid . '/OutgoingCallerIds/' . $CallerIDSid;
+					 	
+		//Form the REST Request URI
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
+		
+		//Send the request and return the result as XML
+		return parent::send_request( array(), $requestURI, "DELETE", DEBUG_MODE, DUMMY_MODE ); 
+		 
+	 }	 
+
+ 	 /**
+	  * view_outgoingcallerid function.
+	  *
+	  * Retreives representation of a specified outgoing caller id.
+	  * 
+	  * @access public 
+	  *
+	  * @method $Numbers->view_outgoingcallerid( $CallerIDSid );
+	  *
+	  * @param string $CallerIDSid - The Sid of the outgoing caller id which we are to view.
+	  * @param string $Sid - The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
+	  *
+	  * @return XML response from API.
+	  */
+	 public function view_outgoingcallerid( $CallerIDSid, $Sid=API_ACCOUNT_SID ) {
+		 
+		//Establish the resource
+		$resource = '/Accounts/' . $Sid . '/OutgoingCallerIds/' . $CallerIDSid;
+					 	
+		//Form the REST Request URI
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
+		
+		//Send the request and return the result as XML
+		return parent::send_request( array(), $requestURI, "GET", DEBUG_MODE, DUMMY_MODE ); 
+		 
+	 }	 
 	 
+
+ 	 /**
+	  * edit_outgoingcallerid function.
+	  *
+	  * Retreives representation of a specified outgoing caller id.
+	  * 
+	  * @access public 
+	  *
+	  * @method $Numbers->edit_outgoingcallerid( $Params, $CallerIDSid );
+	  *
+	  * @param array $Params - An array e.g, (ParameterName => Value). See https://www.twilio.com/docs/api/rest/outgoing-caller-ids#instance-post-optional-parameters
+	  * @param string $CallerIDSid - The Sid of the outgoing caller id which we are to edit.
+	  * @param string $Sid - The account Sid to perform this request on. Defaults to the master Sid if left blank.	  
+	  *
+	  * @return XML response from API.
+	  */
+	 public function edit_outgoingcallerid( $Params = array(), $CallerIDSid, $Sid=API_ACCOUNT_SID ) {
+		 
+		//Establish the resource
+		$resource = '/Accounts/' . $Sid . '/OutgoingCallerIds/' . $CallerIDSid;
+					 	
+		//Form the REST Request URI
+		$requestURI = $this->api_protocol . $this->api_location . $resource;	
+		
+		//Send the request and return the result as XML
+		return parent::send_request( $Params, $requestURI, "POST", DEBUG_MODE, DUMMY_MODE ); 
+		 
+	 }
 	 
-	 
+	 	
  
  }
 
